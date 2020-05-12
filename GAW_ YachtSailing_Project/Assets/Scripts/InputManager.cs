25c49c026d770e986bb9031553f1b9467d2de3c3
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-05-12 23:04:18 +0900

    ボートのモデル作成とInputManager追加

---
+++ GAW_ YachtSailing_Project/Assets/Scripts/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] BoatController boatController;
+    BoatControls inputs;
+
+    private void Awake() => inputs = new BoatControls();
+
+    private void OnEnable() => inputs.Enable();
+    private void OnDestroy() => inputs.Disable();
+    private void OnDisable() => inputs.Disable();
+
+    // Update is called once per frame
+    void Update()
+    {
+        boatController.MoveVec2 = inputs.Player.Move.ReadValue<Vector2>();
+    }
+}
